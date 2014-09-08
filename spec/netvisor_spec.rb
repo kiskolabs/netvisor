0d2984fe9f88d7a3156504d16a7934df907347bb
Timo Sand <timo.sand@eficode.com>
2014-09-08 16:16:26 +0300

    Fix test run typo

--- spec/netvisor_spec.rb
+++ spec/netvisor_spec.rb
@@ -1,4 +1,5 @@
-require "spec_helper"
+require 'spec_helper'
+require 'netvisor'
 
 describe Netvisor do
   describe "#configure" do
@@ -9,7 +10,7 @@ describe Netvisor do
     end
 
     it "returns a url for foo.bar" do
-      url = Netvisor::Request.build_url('baz', nil, nil))
+      url = Netvisor::Request.build_url('baz', nil, nil)
 
       expect(url).to be_a(String)
       expect(url).to eq('http://foo.bar/baz.nv')
