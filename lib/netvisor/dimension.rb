abb293bf1b3c2bacd84213ba8ce489f89b4d6f14
Dmitry Gusev <dmitry@kiskolabs.com>
2022-08-05 16:28:42 +0300

    wrap dimension class with Netvisor module

--- lib/netvisor/dimension.rb
+++ lib/netvisor/dimension.rb
@@ -1,8 +1,10 @@
 require 'happymapper'
 
-class Dimension
-  include HappyMapper
+module Netvisor
+  class Dimension
+    include HappyMapper
 
-  element :name, String, :tag => 'DimensionName'
-  element :item, String, :tag => 'DimensionItem'
+    element :name, String, :tag => 'DimensionName'
+    element :item, String, :tag => 'DimensionItem'
+  end
 end
