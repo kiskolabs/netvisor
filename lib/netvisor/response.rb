3c319af3fc14aff6f549b994d81deee9f80de5a7
Tien Nguyen <ttienism@gmail.com>
2023-11-21 15:49:40 +0200

    Add sales invoice list details for fetching

--- lib/netvisor/response.rb
+++ lib/netvisor/response.rb
@@ -75,13 +75,37 @@ module Netvisor
       element :invoice_lines, SalesInvoiceLines, :tag => 'InvoiceLines'
     end
 
+    class SalesInvoiceList
+      include HappyMapper
+
+      class SalesInvoice
+        include HappyMapper
+
+        tag self.name.split('::').last
+
+        element :netvisor_key, String, :tag => 'NetvisorKey'
+        element :sales_invoice_number, String, :tag => 'InvoiceNumber'
+        element :sales_invoice_date, String, :tag => 'InvoiceDate'
+        element :sales_invoice_status, String, :tag => 'InvoiceStatus'
+        element :customer_code, String, :tag => 'CustomerCode'
+        element :customer_name, String, :tag => 'CustomerName'
+        element :reference_number, String, :tag => 'ReferenceNumber'
+        element :invoice_sum, Float, :tag => 'InvoiceSum'
+        element :open_sum, Float, :tag => 'OpenSum'
+        element :uri, String, :tag => 'Uri'
+      end
+
+      has_many :sales_invoices, SalesInvoice, :tag => 'SalesInvoice'
+    end
+
     tag 'Root'
 
-    element :replies,       Replies
-    element :status,        ResponseStatus
-    element :product_list,  ResponseProductList, :tag => 'ProductList'
-    element :customer,      Customer, :tag => 'Customer'
-    element :customer_list, ResponseCustomerList, :tag => 'Customerlist'
-    element :sales_invoice, SalesInvoice, :tag => 'SalesInvoice'
+    element :replies,            Replies
+    element :status,             ResponseStatus
+    element :product_list,       ResponseProductList, :tag => 'ProductList'
+    element :customer,           Customer, :tag => 'Customer'
+    element :customer_list,      ResponseCustomerList, :tag => 'Customerlist'
+    element :sales_invoice,      SalesInvoice, :tag => 'SalesInvoice'
+    element :sales_invoice_list, SalesInvoiceList, :tag => 'SalesInvoiceList'
   end
 end
